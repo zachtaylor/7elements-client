4e55ba232e4cd111f3dcf0808e6c56c792a07555
Zach Taylor <zach@ztaylor.me>
2021-08-31 23:53:53 -0500

    fix toggle initially =true, better toggle state representation

--- src/app/toggle/toggle.component.ts
+++ src/app/toggle/toggle.component.ts
@@ -1,18 +1,37 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
+import { AfterViewInit, Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core'
 
 @Component({
   selector: 'toggle',
   templateUrl: './toggle.component.html',
   styleUrls: ['./toggle.component.less']
 })
-export class ToggleComponent {
-  @Input() check = false
+export class ToggleComponent implements AfterViewInit {
+
+  @ViewChild('input')
+  private input : ElementRef
+
+  // _check is store for awaiting ViewChild
+  private _check: boolean
+
+  get check() : boolean {
+    if (this.input) return this.input.nativeElement.checked
+    return this._check
+  }
+  @Input() set check(check : boolean) {
+    if (this.input) this.input.nativeElement.checked = check
+    else this._check = check
+  }
+
   @Output() checkChange = new EventEmitter<boolean>()
 
   constructor() { }
 
-  toggle(event) {
-    event.stopPropagation()
+  ngAfterViewInit() {
+    if (this._check) this.check = true
+  }
+
+  toggle($event) {
+    if ($event) $event.stopPropagation()
     this.check = !this.check
     this.checkChange.emit(this.check)
   }
